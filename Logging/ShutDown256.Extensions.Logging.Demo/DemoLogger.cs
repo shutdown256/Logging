1641fb64cc1d3567cde21740e0122ab343bcf209
Vladislav Richter <ShutDown256@Gmail.com>
2017-09-15 00:53:03 +0200

    added demo

---
+++ Logging/ShutDown256.Extensions.Logging.Demo/DemoLogger.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShutDown256.Extensions.Logging.Demo {
+    class DemoLogger : ILogger {
+        public IDisposable BeginScope<TState>(TState state) => new DummyScope();
+        public bool IsEnabled(LogLevel logLevel) => true;
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) {
+            var message = formatter(state, exception);
+            var names = string.Join(", ", (state as IReadOnlyList<KeyValuePair<string, object>>).Select(kvp => kvp.Key));
+            Console.WriteLine($"{message} => [{names}]");
+        }
+
+        class DummyScope : IDisposable {
+            public void Dispose() { }
+        }
+    }
+}
---
+++ Logging/ShutDown256.Extensions.Logging.Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ShutDown256.Extensions.Logging;
+
+namespace ShutDown256.Extensions.Logging.Demo {
+    class Program {
+        static void Main(string[] args) {
+            var logger = new DemoLogger();
+            var str1 = "Foo";
+            var str2 = "Bar";
+            var i = 5;
+            var d = 0.33;
+            logger.LogInformation($"Simple message without any interpolation");
+            logger.LogInformation($"Message '{str1}' without naming");
+            logger.LogInformation($"Message '{str1:Foo}' with name Foo");
+            logger.LogInformation($"Message '{str1:Foo,15}' with name Foo and padding");
+            logger.LogInformation($"Message '{str1:Foo}' with name Foo and another message '{str2}' without name");
+            logger.LogInformation($"Value '{i:_:00}' without naming but still formatted");
+            logger.LogInformation($"Value '{i:number:00}' named number and formatted");
+            logger.LogInformation($"Value '{d:FP,3:#.0}' named number and formatted");
+            logger.LogInformation($"It is '{DateTime.Now:now:t}' and the message #{i:num:00} is '{str1:message}'");
+            Console.ReadKey();
+        }
+    }
+}
